a87a9518f1c049e32e8bea1140d5f20d2ee4e366
Daniel de Santos Sierra <desantossierra@gmail.com>
2022-07-25 10:32:42 +0200

    #1: requirements installation (sync)

--- scripts/update-env.sh
+++ scripts/update-env.sh
@@ -3,6 +3,7 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 PYENV=`pyenv -v`
 
@@ -12,5 +13,7 @@ then
     sh scripts/pyenv-installation.sh
 fi
 
+pyenv virtualenv 3.9.2 dragon
 pyenv activate dragon
 
+pip install -r requirements.txt
